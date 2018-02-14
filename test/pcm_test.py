ee670a6b14dea0e082623efdfd989bc5098a2be6
Nikolay Markov <a4blank@yahoo.com>
2018-02-14 17:22:43 +0000

    delete test

--- test/pcm_test.py
+++ test/pcm_test.py
@@ -9,6 +9,11 @@ class TestPcm(unittest.TestCase):
         pcm.add_entry('scholar_1.txt')
         self.assertEqual(1, len(pcm.all_entries), 'Should be 1 entry')
 
+    def test_delete_entry(self):
+        pcm.add_entry('scholar_1.txt')
+        pcm.delete_entry(pcm.all_entries[0]['uuid'])
+        self.assertEqual(0, len(pcm.all_entries), 'Should be empty')
+
 
 if __name__ == '__main__':
     unittest.main()
